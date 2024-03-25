f2cf6865c6160ef7251ebcecc923e2c545d5d4c9
naweed08 <naweedshah08@gmail.com>
2024-03-25 11:27:00 +0000

    Changing file type to .java

---
+++ Champion.java
@@ -0,0 +1,12 @@
+package cwk4;
+
+public class Champion {
+
+    private String name;
+    private int championID;
+    private int SkillLevel;
+    private int entryFee;
+    private String state;
+
+
+}
