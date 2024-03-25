8f0e426e1a5e58d4c6721c5ff5d97288ca5168da
naweed08 <naweedshah08@gmail.com>
2024-03-25 11:50:42 +0000

    Working on Vizier Class

--- Vizier.java
+++ Vizier.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class Vizier{
     private String name;
     private int treasury = 1000;
-
     private ArrayList<Integer> team;
     private boolean hasChampions;
     private boolean isTreasuryInDebt;
