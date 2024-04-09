bd31e2cb85222a298a6fad3bf27aeda955b143ff
paulmile <164000070+paulmile@users.noreply.github.com>
2024-04-09 11:09:35 +0100

    Update Warrior.java
    -first draft of warrior class

--- Warrior.java
+++ Warrior.java
@@ -1,7 +1,48 @@
-package cwk4;
 
-public class Warrior extends Champion{
+/**
+ * Write a description of class Warrior here.
+ *
+ * @author (your name)
+ * @version (a version number or a date) add subclass necromancer
+ */
+public class Warrior extends Champion {
     private String weapon;
-
+    private final int entryFee;
+    
+    /** constructor
+     * @param nme - name of the warrior
+     * @param skill - skill level
+     * @param weapon - the name of the preferred weapon
+     */ 
+     public Warrior(String nme, int skill, String weapon)
+     {
+         super(nme, skill);
+          this.weapon = weapon;
+          this.entryFee = calculateEntryFee();
+     }
+    
+     /** 
+     * 
+     * @return the warrior entry fee
+     */
+     public int calculateEntryFee()
+     {
+         int fee = super.getSkillLvl() * 100;
+         return fee;
+     }
+     
+     @Override
+     /** Implements toString from Champion class
+     * 
+     * @return the wizard information as a String
+     */
+     public String toString()
+     {
+          String s = "Class: Warrior || State: " +
+          super.toString() +
+          "Preferred weapon: " + this.weapon + "\n" +
+          "Entry Fee: " + this.entryFee + "\n";
+          return s;
+     }
 
 }
