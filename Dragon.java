b74be564895a3c4fd0f4f6d9633fb07d1fe22f91
CherryCrave <154535790+CherryCrave@users.noreply.github.com>
2024-04-19 13:30:49 +0100

    Update Dragon.java
    Fixed 'this.' implementation

--- Dragon.java
+++ Dragon.java
@@ -1,31 +1,33 @@
+package cwk4;
+
 public class Dragon extends Champion{
-     private boolean canTalk;
-     private final int entryFee;
-     
+    private boolean canTalk;
+    private final int entryFee;
+
     /** constructor
-     * @param nme - name of the dragon
-     * @param talk - boolean to indicate whether the dragon can talk
-     */ 
-     public Dragon(String nme, boolean canTalk)
-     {
-          super(nme, 7);
-          this.canTalk = canTalk;
-          this.entryFee = 500;
-     }
-    
-     
+     * @param name - name of the dragon
+     * @param canTalk - boolean to indicate whether the dragon can talk
+     */
+    public Dragon(String name, boolean canTalk)
+    {
+        super(name, 7);
+        this.canTalk = canTalk;
+        this.entryFee = 500;
+    }
+
+
     @Override
-     /** Implements toString from Champion class
-     * 
+    /** Implements toString from Champion class
+     *
      * @return the dragon information as a String
      */
-     public String toString()
-     {
-          String s = "Class: Dragon || State: " +
-          super.toString() +
-          "Can Talk: " + canTalk + "\n" +
-          "Entry Fee: " + this.entryFee + "\n";
-          return s;
-     }
-     
+    public String toString()
+    {
+        String s = "Class: Dragon || State: " +
+                super.toString() +
+                "Can Talk: " + canTalk + "\n" +
+                "Entry Fee: " + this.entryFee + "\n";
+        return s;
+    }
+
 }
