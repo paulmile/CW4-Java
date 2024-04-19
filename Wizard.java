efb9572aa6e61e2ad5bfd486e00ba325c234299c
CherryCrave <154535790+CherryCrave@users.noreply.github.com>
2024-04-19 13:32:27 +0100

    Update Wizard.java

--- Wizard.java
+++ Wizard.java
@@ -1,3 +1,4 @@
+package cwk4;
 
 /**
  * Write a description of class Wizard here.
@@ -9,49 +10,48 @@ public class Wizard extends Champion {
     private String specialitySpell;
     private boolean isNecromancer;
     private final int entryFee;
-    
+
     /** constructor
-     * @param nme - name of the wizard
+     * @param name - name of the wizard
      * @param skill - skill level
-     * @param fee - fee amount
-     * @param isNecro - true if the wizard is also a necromancer
+     * @param isNecromancer - true if the wizard is also a necromancer
      * @param spell - the name of the speciality spell
-     */ 
-     public Wizard(String nme, int skill, boolean isNecromancer, String spell)
-     {
-         super(nme, skill);
-          this.isNecromancer = isNecromancer;
-          this.specialitySpell = spell;
-          this.entryFee = calculateEntryFee();
-     }
-    
-     /** Implements setEntryFee from Champion class
-     * 
+     */
+    public Wizard(String name, int skill, boolean isNecromancer, String spell)
+    {
+        super(name, skill);
+        this.isNecromancer = isNecromancer;
+        this.specialitySpell = spell;
+        this.entryFee = calculateEntryFee();
+    }
+
+    /** Implements setEntryFee from Champion class
+     *
      * @return the wizard entry fee
      */
-     public int calculateEntryFee()
-     {
-         if (this.isNecromancer)
-         {
+    public int calculateEntryFee()
+    {
+        if (this.isNecromancer)
+        {
             return 400;
-         }else{
+        }else{
             return 300;
-         }
-     }
-     
-     @Override
-     /** Implements toString from Champion class
-     * 
+        }
+    }
+
+    @Override
+    /** Implements toString from Champion class
+     *
      * @return the wizard information as a String
      */
-     public String toString()
-     {
-          String s = "Class: Wizard || State: " +
-          super.toString() +
-          "Necromancer: " + isNecromancer + "\n" +
-          "Speciality spell: " + this.specialitySpell + "\n" +
-          "Entry Fee: " + this.entryFee + "\n";
-          return s;
-     }
+    public String toString()
+    {
+        String s = "Class: Wizard || State: " +
+                super.toString() +
+                "Necromancer: " + isNecromancer + "\n" +
+                "Speciality spell: " + this.specialitySpell + "\n" +
+                "Entry Fee: " + this.entryFee + "\n";
+        return s;
+    }
 
 }
